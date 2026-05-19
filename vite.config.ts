d714f46144242419594fcdad03803e8cf30c37b1
Apri Pandu W <sarahpalastrin@gmail.com>
2026-05-19 13:35:25 +0700

    chore(config): update project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
-import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
-import { solidStart } from "@solidjs/start/config";
+import { defineConfig } from 'vite';
+import { nitroV2Plugin as nitro } from '@solidjs/vite-plugin-nitro-2';
+import { solidStart } from '@solidjs/start/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    solidStart(),
-    nitro()
+    tailwindcss(),
+    solidStart({
+      middleware: './src/middleware/security.ts',
+    }),
+    nitro(),
   ],
-  ssr: { external: ["@prisma/client"] }
+  ssr: { external: ['@prisma/client'] },
 });
